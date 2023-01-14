27dd58a025ee2473da30c889c9097156c237a322
unknown <yassine.ouri7319@gmail.com>
2023-01-14 20:52:15 +0100

    fetched coffee stores using foursquare API

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,17 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router'
 import React from 'react'
 import CoffeeStores from '../../public/data/coffee-store.data';
 
+import NearMeIcon from "../../public/icons/nearMe.svg"
+import StarIcon from "../../public/icons/star.svg"
+import PlacesIcon from "../../public/icons/places.svg"
+
+import cls from "classnames"
+
+import styles from '../../styles/coffee-store.module.css'
 
 export function getStaticProps({params}) {
   return {
@@ -22,22 +30,59 @@ export function getStaticPaths() {
   }
 }
 
+
+const handleUpvote = () => {
+  console.log('upvoted')
+}
+
 const CoffeeShop = ({coffeeStore}) => {
 
-  const {name, adress, neighbourhood, imgUrl} = coffeeStore
+  const {name, address, neighbourhood, imgUrl} = coffeeStore
 
   const router = useRouter();
   if(router.isFallback){
     return <div>Loading...</div>
   }
   return (
-    <>
-    <Head>
-      <title>{name}</title>
-    </Head>
-    <Link href='/' prefetch>Back to home</Link>
-    </>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href='/'  prefetch>← Back to home</Link>
+          </div>
+          <h1 className={styles.name}>{name}</h1>
+          <Image 
+            src={imgUrl} 
+            width={600} 
+            height={460} 
+            className={styles.storeImg} 
+            alt={name} />
+        </div>
+        <div className={cls(styles.col2, "glass" )}>
+          <div className={styles.iconWrapper}>
+            <Image src={PlacesIcon} width={24} height={24} />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src={NearMeIcon} width={24} height={24} />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src={StarIcon} width={24} height={24} />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvote}>
+            Upvote !
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
+
+// fsq3C4Wxnx9Fitn7IzjRxCX1hmUBdKeGeynTdMttTrz/Gnc=
 
 export default CoffeeShop
--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,34 @@ import styles from '../styles/Home.module.css'
 import Banner from '../Components/banner'
 import HeroImage from '../public/hero-image-3.png'
 import Card from '../Components/card'
-import {CoffeeImg} from '../public/coffee-exmple.jpg'
-import data from '../public/data/coffee-store.data'
+// import data from '../public/data/coffee-store.data'
+
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export async function getStaticProps(context) {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'fsq3C4Wxnx9Fitn7IzjRxCX1hmUBdKeGeynTdMttTrz/Gnc='
+    }
+  };
+  const response = await fetch('https://api.foursquare.com/v3/places/nearby?ll=36.7978152,10.1832781&query=coffee stores', options);
+  const data = await response.json();
+  console.log(data)
   return {
     props : {
-      data,
+      data : data.results,
     }
   }
 }
 
-export default function Home(props ) {
+export default function Home(props) {
 
   let buttonText = "View Stores Nearby";
-
+  console.log(props.data)
   const handleClick = () => {
     buttonText = "Loading..."
     console.log("clicked")
@@ -46,12 +57,12 @@ export default function Home(props ) {
           }
         <div className={styles.cardLayout}>
           {
-            data.map(store => (
+            props.data.map(store => (
               <Card
-                key={store.id}
+                key={store.fsq_id}
                 name={store.name}
-                imgUrl = {store.imgUrl}
-                href = {`/coffee-store/${store.id}`}
+                imgUrl = {store.imgUrl || ""}
+                href = {`/coffee-store/${store.fsq_id}`}
                 websiteUrl = {store.websiteUrl}
                 address = {store.address}
                 />
